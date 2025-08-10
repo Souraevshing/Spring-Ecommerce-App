48e6e33a86221a19b6574bfc5c446aa5d75ff858
Souraevshing <isauravshing@gmail.com>
2025-08-11 00:26:53 +0530

    feat: add product entity and dto

---
+++ ecommerce/src/main/java/com/ecomm/ecommerce/dto/ProductDto.java
@@ -0,0 +1,51 @@
+package com.ecomm.ecommerce.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class ProductDto {
+
+    private Long id;
+
+    @NotBlank(message = "Product name is required")
+    @Size(max = 50, message = "Product name must be most 50 characters")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    @Size(max = 50, message = "Description must be most 50 characters")
+    private String description;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive")
+    @Digits(integer = 20, fraction = 2)
+    @DecimalMin(value = "1.00", message = "Price must be at least 1.00")
+    private BigDecimal price;
+
+    @NotNull(message = "Stock quantity is required")
+    @Min(value = 0, message = "Stock quantity cannot be negative")
+    private Integer stockQuantity;
+
+    @NotBlank(message = "Category is required")
+    @Size(max = 50, message = "Category must be at most 50 characters")
+    private String category;
+
+    @NotBlank(message = "Image URL is required")
+    @Size(max = 50, message = "Image URL must be at most 50 characters")
+    private String imageUrl;
+
+    private final Boolean active = true;
+
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime updatedAt;
+
+}
---
+++ ecommerce/src/main/java/com/ecomm/ecommerce/entity/ProductEntity.java
@@ -0,0 +1,49 @@
+package com.ecomm.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "product_tab")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 250)
+    private String description;
+
+    @Column(name = "price", nullable = false, length = 20, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "stock_quantity", nullable = false)
+    private Integer stockQuantity;
+
+    @Column(name = "category", nullable = false, length = 20)
+    private String category;
+
+    @Column(name = "image_url", nullable = false, length = 50)
+    private String imageUrl;
+
+    @Column(name = "active", nullable = false)
+    private Boolean active = true;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
